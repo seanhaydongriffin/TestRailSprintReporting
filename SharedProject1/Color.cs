a50b34382665fb175c77708629a0394ef48bf475
Sean Griffin <seangriffin@internode.on.net>
2022-09-11 23:23:21 +1000

    Add files via upload

---
+++ SharedProject1/Color.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SharedProject
+{
+    public static class Color
+    {
+        public static string NameToHex(string name)
+        {
+            int ColorValue = System.Drawing.Color.FromName(name).ToArgb() & 0xFFFFFF;
+            return string.Format("#{0:X6}", ColorValue);
+        }
+    }
+}
