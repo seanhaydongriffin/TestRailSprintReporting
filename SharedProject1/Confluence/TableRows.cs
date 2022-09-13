c5c3ff6704f43c65b511d5d0e349225e99d55823
Sean Griffin <seangriffin@internode.on.net>
2022-09-13 14:52:53 +1000

    Add files via upload

---
+++ SharedProject1/Confluence/TableRows.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace SharedProject.Confluence
+{
+    public class TableRows
+    {
+
+        private List<XElement> _TableRows;
+
+        public TableRows()
+        {
+            _TableRows = new List<XElement>();
+        }
+
+        public List<XElement> GetList()
+        {
+            return _TableRows;
+        }
+
+        public TableRows Add(XElement row)
+        {
+            _TableRows.Add(row);
+            return this;
+        }
+
+        public TableRows AddHeader(params string[] fields)
+        {
+            var row = new XElement("tr");
+
+            foreach (var field in fields)
+
+                row.Add(new XElement("th", new XElement("p", field)));
+
+            _TableRows.Add(row);
+            return this;
+        }
+
+        public TableRows Add(params string[] fields)
+        {
+            var row = new XElement("tr");
+
+            foreach (var field in fields)
+
+                row.Add(new XElement("td", new XElement("p", field)));
+
+            _TableRows.Add(row);
+            return this;
+        }
+
+        public TableRows Add(params object[] fields)
+        {
+            var row = new XElement("tr");
+
+            foreach (var field in fields)
+
+                row.Add(new XElement("td", new XElement("sub", field)));
+
+            _TableRows.Add(row);
+            return this;
+        }
+
+
+    }
+}
